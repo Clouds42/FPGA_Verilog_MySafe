57f90299ef03e63007fb6d87186ed1521cbe43b0
Clouds42 <1577338630@qq.com>
2019-10-06 14:47:35 +0800

    MySafe_v0.4
    Unnecessary lines removed.
    Variable names standardized.

--- 00MyPassword00.v
+++ 00MyPassword00.v
@@ -1,35 +1,27 @@
-module pwd (rst,sw_pwd,LED_right,LED_wrong,clk,key_confirm);
+module pwd (key_refresh,sw_pwd,LED_right,LED_wrong,clk);
  
-	input rst;
-	input [3:0] sw_pwd;
-	input key_confirm;
-	input clk;
-	output LED_right;
-	output LED_wrong;
+	input clk;//时钟.
+	input key_refresh;//状态刷新.
+	input [3:0] sw_pwd;//开关.
+	output LED_right;//密码正确指示灯.
+	output LED_wrong;//密码错误指示灯.
  
-	parameter password = 4'b1001;
+	parameter password = 4'b1001;//密码为1001,对应四路拨动开关.
  
-	reg alert;
-	reg key_pressed;
+	reg flag;//储存密码是否正确,0或1.
  
-	always@(posedge clk)
+	always@(posedge clk)//时钟边沿触发.
 	begin
-		if(!rst)
-		begin
-			alert<=1'b0;
-			key_pressed<=1'b0;
-		end
-		else if(key_confirm && ~key_pressed)
-		begin
-			key_pressed<=1'b1;
-			if(sw_pwd==password)
-				alert<=1'b0;
-			else
-				alert<=1'b1;
-		end
+		if(!key_refresh)//状态刷新.
+			begin
+				if(sw_pwd==password)
+					flag<=1'b0;//密码正确.
+				else
+					flag<=1'b1;//密码错误.
+			end
 	end
- 
-		assign LED_wrong=~(alert&key_pressed);
-		assign LED_right=~((~alert)&key_pressed);
+	
+	assign LED_right=flag;//密码正确亮绿灯.
+	assign LED_wrong=~flag;//密码错误亮红灯.
  
 endmodule
